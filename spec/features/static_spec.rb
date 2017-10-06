01a90531d01e2b93740358c4d38f0e4ddf941af2
Thomas Carney <tommycarney@gmail.com>
2017-10-06 12:22:08 +0200

    Add rspec, capybara and setup homepage

---
+++ spec/features/static_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe "navigate" do
+  describe "homepage" do
+    it "can be reached succesfully" do
+      visit root_path
+      expect(page.status_code).to eq(200)
+    end
+  end
+end
