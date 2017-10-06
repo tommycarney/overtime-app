e130d31120bbcdfe00faf023e70ee0d3c07d678e
Thomas Carney <tommycarney@gmail.com>
2017-10-06 14:24:46 +0200

    Used the resource generator for posts

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   describe "creation" do
     before do
-      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+      @user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Thomas", last_name: "Carney")
     end
     it "can be created" do
       expect(@user).to be_valid
