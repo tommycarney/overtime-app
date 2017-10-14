ba273584318fe88a4e253c8cf56902bd05831a22
Thomas Carney <tommycarney@gmail.com>
2017-10-14 10:12:49 +0200

    Integrated application for status and implemented enums for audit log

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -1,7 +1,7 @@
 class AuditLog < ApplicationRecord
   belongs_to :user
   validates_presence_of :user_id, :status, :start_date
-
+  enum status: { pending: 0, confirmed: 1 }
   after_initialize :set_defaults
 
 
