d5b7dbd75a64d342942c27f87248671db91f41d3
Thomas Carney <tommycarney@gmail.com>
2017-10-16 14:54:28 +0200

    Builds out rake task for sending sms messages and STI for employees

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
-@user = User.create!(email: "test@test.com",
+@employee = Employee.create!(email: "test@test.com",
                     password: "asdfasdf",
                     password_confirmation: "asdfasdf",
                     first_name: "Thomas",
                     last_name: "Carney",
-                    phone: "1234567891")
+                    phone: "1638687560")
 
 AdminUser.create!(email: "admin@test.com",
                   password: "asdfasdf",
                   password_confirmation: "asdfasdf",
                   first_name: "Thomas",
                   last_name: "Carney",
-                  phone: "1234567891")
+                  phone: "1638687560")
 
 puts "1 user created"
 
-AuditLog.create!(user_id: @user.id, start_date: (Date.today - 6.days))
-AuditLog.create!(user_id: @user.id, start_date: (Date.today - 1.weeks) - 6.days)
-AuditLog.create!(user_id: @user.id, start_date: (Date.today - 2.weeks) - 6.days)
+AuditLog.create!(user_id: @employee.id, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @employee.id, start_date: (Date.today - 1.weeks) - 6.days)
+AuditLog.create!(user_id: @employee.id, start_date: (Date.today - 2.weeks) - 6.days)
 
 
 puts "3 audit log have been created"
@@ -25,7 +25,7 @@ puts "3 audit log have been created"
 
   Post.create!(date: Date.today, rationale: "#{post} Lorem ipsum dolor sit amet, no putent eligendi per, eam ipsum malorum an. No ius prima ubique postea, virtute constituam ea qui. Duo eu altera ponderum adipiscing, sed cu homero probatus. Id pri iudico legendos dignissim. Est an veniam tritani scripserit, pro clita virtute perpetua no.
 
-Debet debitis scaevola eam id, cum id debet scripta, ut mollis electram quo. Vix omnium nostrum an, mandamus patrioque comprehensam mei ei. Quot iuvaret cum ut. Audire commune ea eam, modo purto utroque vim cu. Fastidii torquatos te mel.", user_id: @user.id, overtime_request: 2.5)
+Debet debitis scaevola eam id, cum id debet scripta, ut mollis electram quo. Vix omnium nostrum an, mandamus patrioque comprehensam mei ei. Quot iuvaret cum ut. Audire commune ea eam, modo purto utroque vim cu. Fastidii torquatos te mel.", user_id: @employee.id, overtime_request: 2.5)
 
 end
 
--- lib/tasks/notification.rake
+++ lib/tasks/notification.rake
@@ -1,13 +1,16 @@
 namespace :notification do
   desc "Sends sms notification to employees asking them to log if they had overtime or not"
   task sms: :environment do
-    # 1. schedule to run at Sunday at 5 pm
-    # 2. Iterate over all employees
-    # 3. Skip admin users
-    # 4. Send a msg that has instructions and link to log time
-    #User.all.each do |user|
-    #  SmsTool.send_sms()
-    #end
+    include Rails.application.routes.url_helpers
+    if Time.now.monday?
+
+      employees = Employee.all
+      notification_message = "Please log into the overtime management dashboard to request overtime or confirm hours for last week: http://localhost:3000"
+
+      employees.each do |employee|
+        SmsTool.send_sms(number: employee.phone, message: notification_message)
+      end
+    end
   end
 
   desc "Sends mail notification to managers (admin users) each day to inform of pending overtime requests"
