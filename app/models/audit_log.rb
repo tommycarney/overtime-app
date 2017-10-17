1dac16b0718d78c62d98914c92e6438372e5cc67
Thomas Carney <tommycarney@gmail.com>
2017-10-17 16:40:43 +0200

    Adds callback to the audit log for end date

--- app/models/audit_log.rb
+++ app/models/audit_log.rb
@@ -3,11 +3,16 @@ class AuditLog < ApplicationRecord
   validates_presence_of :user_id, :status, :start_date
   enum status: { pending: 0, confirmed: 1 }
   after_initialize :set_defaults
+  before_update :set_end_date, if: :confirmed?
 
 
   private
 
     def set_defaults
       self.start_date ||= Date.today - 6.days
+    end
+
+    def set_end_date
+      self.end_date = Date.today
     end
 end
